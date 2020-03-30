d4847ac82182ad377428bc68c60cd1517fde6c83
cpawamgit <cyril.morin.tai@gmail.com>
2020-03-30 12:25:59 +0200

    winenv add to build on windows

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = ['pygame'], excludes = [], include_files = ['env/lib/python3.6/site-packages/pygame/', 'gamefiles', 'mcclasses'])
+
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
+
+if sys.platform=='win32':
+    buildOptions = dict(packages = ['pygame'], excludes = [], include_files = ['env/lib/python3.6/site-packages/pygame/', 'gamefiles', 'mcclasses'])
+else:
+    buildOptions = dict(packages = ['pygame'], excludes = [], include_files = ['winenv/Lib/site-packages/pygame/', 'gamefiles', 'mcclasses'])
 
 executables = [
     Executable('mac.py', base=base)
